4ec2de1c45d4cb31898713e2c88033d2ad6c3380
motaz <romal78@hotmail.com>
2024-07-12 11:30:56 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,7 @@
+const {sum} = require('./sum.js'); // sum = require('./sum.js'); // importamos el modulo
+console.log('HOLA MUNDO');
+//console.log(Window);
+// globalThis.
+//console.log(globalThis);
+
+console.log(sum(10,20));
---
+++ sum.js
@@ -0,0 +1,7 @@
+function sum(a,b) {
+    return a + b;
+    
+}
+//module.exports = sum; // exportamos el modulo
+// exportar el modulo como objeto
+module.exports = {sum} //  exportamos el modulo
